f1cb895eea123d6807eb06957f5b2d4428fd843a
Ariel Rossanigo <arielrossanigo@gmail.com>
2017-03-27 00:32:18 -0300

    refactor and costs for votes

--- scheduler.py
+++ scheduler.py
@@ -20,6 +20,9 @@ class PyCampScheduleProblem:
     def __init__(self, data):
         self.data = munchify(data)
         self.project_list = list(self.data.projects.keys())
+        self.total_participants = len(set([vote
+                                           for pr in self.data.projects.values()
+                                           for vote in pr.votes]))
 
     def neighboors(self, state):
         ''''Returns the list of neighboors of the state'''
@@ -36,34 +39,63 @@ class PyCampScheduleProblem:
 
     def value(self, state):
         '''Returns the objective value of the state'''
-        cost = 0
+        responsables_collisions_cost = 0
+        participant_collisions_cost = 0
+        responsable_not_available_cost = 0
+        most_voted_cost = 0
+        slot_population_cost = 0
+        project_not_in_priority_slot_cost = 0
 
         slots_and_projects = OrderedDict()
         for slot in self.data.available_slots:
             slots_and_projects[slot] = [proj for proj, proj_slot in state if proj_slot == slot]
 
-        # Cost for having responsables collisions
-        for slot, slot_projects in slots_and_projects.items():
+        for slot_number, (slot, slot_projects) in enumerate(slots_and_projects.items()):
             for proj1, proj2 in combinations(slot_projects, 2):
                 set_resp_1 = set(self.data.projects[proj1].responsables)
                 set_resp_2 = set(self.data.projects[proj2].responsables)
-                if len(set_resp_1.intersection(set_resp_2)) > 0:
-                    cost += IMPOSIBLE_COST
 
-        # Cost for having projects in slots where responsables are not available
+                # Cost for having responsables collisions
+                if len(set_resp_1.intersection(set_resp_2)) > 0:
+                    responsables_collisions_cost += IMPOSIBLE_COST
+
+                # Cost for having voters collisions
+                set_vot_1 = set(self.data.projects[proj1].votes)
+                set_vot_2 = set(self.data.projects[proj2].votes)
+                votes_collisions = len(set_vot_1.intersection(set_vot_2))
+                participant_collisions_cost += votes_collisions
+
+            # Cost for having multiple projects in the same slot and preference
+            # for more occupadied slots at the begining
+            project_quantity = len(slot_projects)
+            slot_population_cost += (3 ** project_quantity) + (slot_number * project_quantity)
+
+            # Cost according to the quantity of votes. It's prefered that most voted projects
+            # were at the begining
+            vote_quantity = sum([len(self.data.projects[project]) for project in slot_projects])
+            most_voted_cost += (slot_number * vote_quantity) / self.total_participants
+
         for project, slot in state:
-            responsables = self.data.projects[project].responsables
+            project_data = self.data.projects[project]
+            # Cost for having projects in slots where responsables are not available
+            responsables = project_data.responsables
             for resp in responsables:
                 if slot not in self.data.responsable_available_slots[resp]:
-                    cost += IMPOSIBLE_COST
+                    responsable_not_available_cost += IMPOSIBLE_COST
 
-        # Cost for having multiple projects in the same slot and preference
-        # for more occupadied slots at the begining
-        for slot_number, (slot, slot_projects) in enumerate(slots_and_projects.items()):
-            project_quantity = len(slot_projects)
-            cost += (3 ** project_quantity) + (slot_number * project_quantity)
+            # Cost for having a project outside priority slots
+            priority_slots = project_data.priority_slots
+            if len(priority_slots) > 0 and slot not in priority_slots:
+                project_not_in_priority_slot_cost += 10
 
-        return -1 * cost
+        return -1 * (
+            responsables_collisions_cost +
+            participant_collisions_cost +
+            responsable_not_available_cost +
+            most_voted_cost +
+            slot_population_cost +
+            project_not_in_priority_slot_cost
+        )
 
     def generate_random_state(self):
         res = []
@@ -76,20 +108,22 @@ class PyCampScheduleProblem:
         sorted_by_slot = sorted(state, key=itemgetter(1))
         lines = []
         for slot in self.data.available_slots:
-            lines.append('+{:-<6s}+{:-<32s}+{:-<32s}+'.format('', '', ''))
+            lines.append('+{:-<6s}+{:-<32s}+{:-<32s}+{:-<4s}+'.format('', '', '', ''))
             slot_project_lines = []
             for project, project_slot in sorted_by_slot:
                 if project_slot == slot:
                     responsables = ', '.join(problem.data.projects[project].responsables)
+                    number_of_votes = len(problem.data.projects[project].votes)
                     slot_project_lines.append(
-                        '|{:^6s}| {:30s} | {:30s} |'.format(slot, project, responsables)
+                        '|{:^6s}| {:30s} | {:30s} | {:2d} |'.format(
+                            slot, project, responsables, number_of_votes)
                     )
 
             if len(slot_project_lines) > 0:
                 lines.extend(slot_project_lines)
             else:
-                lines.append('|{:^6s}|{:32s}|{:32s}|'.format(slot, '', ''))
-        lines.append('+{:-<6s}+{:-<32s}+{:-<32s}+'.format('', '', ''))
+                lines.append('|{:^6s}|{:32s}|{:32s}|{:4s}|'.format(slot, '', '', ''))
+        lines.append('+{:-<6s}+{:-<32s}+{:-<32s}+{:-<4s}+'.format('', '', '', ''))
         print('\n'.join(lines))
 
 
@@ -138,5 +172,5 @@ if __name__ == '__main__':
     problem = PyCampScheduleProblem(data)
     best_solution = random_restart_hill_climbing(problem,
                                                  max_iters=10000,
-                                                 max_iters_without_improvement=10)
+                                                 max_iters_without_improvement=5)
     problem.print_state(best_solution)
